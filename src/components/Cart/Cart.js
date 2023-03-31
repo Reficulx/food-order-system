13a99076b65f64178f8f0939cc766e13dccdac4c
reficulx <home_ai@icloud.com>
2023-03-31 00:34:16 -0700

    cart item removal, addition and output

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import classes from './Cart.module.css';
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
-  const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name: 'Sushi', quantity: 2, price: 12.99}].map(item => <li>{item.name}</li>)}</ul>;
+  const context = useContext(CartContext);
+  const totalAmount = `$${context.totalAmount.toFixed(2)}`;
+  const hasItems = context.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    context.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    context.addItem({...item, quantity: 1});
+  };
+
+  // Remark: use .bind() to make sure the bound parameter is executed when the function is called
+  const cartItems = <ul className={classes['cart-items']}>
+    {context.items.map(item => (
+      <CartItem key={item.id} name={item.name} quantity={item.quantity} price={item.price}
+                onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)}/>
+    ))}
+  </ul>;
   return <Modal onClose={props.onHidingCart}>
     {cartItems}
     <div className={classes.total}>
       <span>Total Amount</span>
-      <span>35.62</span>
+      <span>{totalAmount}</span>
     </div>
     <div className={classes.actions}>
       <button className={classes['button--alt']} onClick={props.onHidingCart}>Close</button>
-      <button className={classes.button}>Order</button>
+      {hasItems && <button className={classes.button}>Order</button>}
     </div>
   </Modal>
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -13,15 +13,42 @@ const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     // .push() operates on the old array
     // .concat() returns a new array
-    const updatedItems = state.items.concat(action.item);
+    let updatedItem;
+    let updatedItems;
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.quantity;
+    // return the index of the found item
+    const existingItemIndex = state.items.findIndex(item => item.id === action.item.id);
+    const existingItem = state.items[existingItemIndex];
+    if (existingItem) {
+      updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + action.item.quantity
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
     };
   } else if (action.type === "REMOVE") {
-    const updatedItems = state.items.remove(action.id);
-
+    let updatedItems;
+    const existingItemIndex = state.items.findIndex(item => item.id === action.id);
+    const existingItem = state.items[existingItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    if (existingItem.quantity === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = {...existingItem, quantity: existingItem.quantity-1};
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
   }
   return defaultCartState;
 };
