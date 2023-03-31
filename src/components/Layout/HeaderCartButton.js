a536fa96708ef95a47b704fa0fc4665d3b1aa06d
reficulx <home_ai@icloud.com>
2023-03-31 00:42:33 -0700

    cart button bump whenever adding new item(s) to the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,29 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   // access the cart context
+  const [isBtnHighlighted, setIsBtnHighlighted] = useState(false);
   const context = useContext(CartContext);
-  const numberOfCartItems = context.items.reduce((quant, item) => quant + item.quantity, 0);
+  const { items } = context;
+  const numberOfCartItems = items.reduce((quant, item) => quant + item.quantity, 0);
 
-  return <button className={classes.button} onClick={props.onClick}>
+  const buttonClasses = `${classes.button} ${isBtnHighlighted ? classes.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setIsBtnHighlighted(true);
+    const timer = setTimeout(() => {
+      setIsBtnHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+  return <button className={buttonClasses} onClick={props.onClick}>
     <span className={classes.icon}>
       <CartIcon />
     </span>
