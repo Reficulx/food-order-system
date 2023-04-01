1b72d8fe74730124dc98fb811fcbfa4d6548373b
reficulx <home_ai@icloud.com>
2023-03-31 23:31:58 -0700

    add error handling function

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,11 +6,15 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   // useEffect should not return a Promise function but a cleanup function, which is synchronous
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(process.env.REACT_APP_BACKEND_URL + "meal.json");
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const data = await response.json();
       const loadedMeals = [];
       for (const key in data) {
@@ -24,13 +28,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     }
 
-    fetchMeals();
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
   if (isLoading) {
     return <section className={classes.MealsLoading}>
       <p>Loading...</p>
     </section>;
   }
+
+  if (httpError) {
+    return <section className={classes.MealsError}>
+      <p>{httpError}</p>
+    </section>
+  }
+
   const mealItems = meals.map(meal => {
     return <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
   });
