7bffa2253b2addd8d43a353a3400cd9085ab6028
reficulx <home_ai@icloud.com>
2023-03-31 23:23:24 -0700

    add backend api connection that provides meals data

---
+++ README.md
@@ -0,0 +1,3 @@
+# Food Order System
+
+This is a food online ordering system completed in React.js with a Firebase backend. To use this system in full functionality please setup the local dev environment to provide backend APIs through Firebase (Please search on google to see how to set it up!).
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,43 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
 const AvailableMeals = () => {
-  const meals = DUMMY_MEALS.map(meal => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect should not return a Promise function but a cleanup function, which is synchronous
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(process.env.REACT_APP_BACKEND_URL + "meal.json");
+      const data = await response.json();
+      const loadedMeals = [];
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          ...data[key]
+        })
+      }
+      console.log(data);
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    }
+
+    fetchMeals();
+  }, []);
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>;
+  }
+  const mealItems = meals.map(meal => {
     return <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
   });
   return <section className={classes.meals}>
     <Card>
-      <ul>
-        {meals}
-      </ul>
+      <ul>{mealItems}</ul>
     </Card>
     </section>
 };
